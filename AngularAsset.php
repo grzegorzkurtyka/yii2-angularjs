2e5afc11ff451e611190bf54c0660dcf4b1aadbe
Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.pl>
2015-04-10 15:21:46 +0200

    debug flag support

--- AngularAsset.php
+++ AngularAsset.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * @link http://www.tesjin.ru/
+ * @link http://schibsted.pl/
  * @copyright Copyright (c) 2014 TesJin Group
+ * @copyright Copyright (c) 2015 Schibsted Tech Polska
  * @license BSD-3-Clause
  */
 
@@ -13,20 +14,33 @@ use yii\web\AssetBundle;
  * This asset bundle provides the [angular javascript library](https://angularjs.org/)
  *
  * @author Vladislav Orlov <orlov@tesjin.ru>
+ * @author Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.pl>
  */
 class AngularAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/tesjin/yii2-angularjs';
-    public $js = [
-        'js/angular.js',
-        'js/angular-animate.js',
-        'js/angular-cookies.js',
-        'js/angular-loader.js',
-        'js/angular-mocks.js',
-        'js/angular-resource.js',
-        'js/angular-route.js',
-        'js/angular-sanitize.js',
-        'js/angular-scenario.js',
-        'js/angular-touch.js',
+    public $sourcePath = '@vendor/grzegorzkurtyka/yii2-angularjs/js';
+
+    public $jsSources = [
+        'angular',
+        'angular-animate',
+        'angular-cookies',
+        'angular-loader',
+        'angular-mocks',
+        'angular-resource',
+        'angular-route',
+        'angular-sanitize',
+        'angular-scenario',
+        'angular-touch',
     ];
+
+    /**
+     * Initializes the bundle.
+     * Depending on debug settings enables non-minimzed versions of the files
+     */
+    public function init() {
+        parent::init();
+        $this->js = array_map(function($js) {
+            return YII_DEBUG ? $js . '.js' : $js . '.min.js';
+        }, $this->jsSources);
+    }
 }
